c8a9a00ac304785bd3063dd3b2dd2b486dee4f0f
kennytat <kennytat.app@gmail.com>
2021-05-20 09:18:57 +0700

    Finalised Setup Project

---
+++ apps/ionic-vgm/src/app/tabs/tabs.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vgm-converter-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss'],
+})
+export class TabsPage {
+  constructor() {}
+}
